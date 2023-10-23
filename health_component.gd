fea977e0253c5bf935e93febdd1d279c21a9d20b
Andrey Bykin <bykinandrey@gmail.com>
2023-10-23 11:56:49 -0400

    HealthComponent Initial sketch
    - Added basic health component
    - Two signals for health change and for on death added

---
+++ health_component.gd
@@ -0,0 +1,34 @@
+extends Node
+
+# Health component is resposible for manageing all things related to health
+# Two Signals are Emitted
+# On Death signal - when health fully runs out
+# On Health Change signal - when health changes either positive or negative
+
+signal on_death
+signal on_health_change(health_change_type)
+
+@export var max_health : int = 100
+@export var current_health : int
+@export var is_damaged : bool = false
+
+func _ready():
+	current_health = max_health
+	
+func heal(amount : int):
+	current_health += amount
+	current_health = min(current_health, max_health)
+	is_damaged = true if current_health < max_health else false
+	on_health_change.emit("Heal")
+
+func take_damage(amount : int):
+	current_health -= amount
+	current_health = max(0, current_health)
+	on_health_change.emit("Damage")
+	is_damaged = true if current_health < max_health else false
+	
+	if current_health <= 0:
+		on_death.emit()
+
+func update_max_health(amount : int):
+	max_health += amount
