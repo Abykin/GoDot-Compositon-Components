17b8b6fd2e69bf677a82f284ecbdd43e53be5807
Andrey Bykin <bykinandrey@gmail.com>
2023-10-27 11:41:02 -0400

    Enemy simple follow on collision with player
    -  Enemy now follows player if the player collides with the enemy hitbox

--- enemy.gd
+++ enemy.gd
@@ -1,12 +1,20 @@
 extends CharacterBody2D
 
+# TODO Add simple patrolling AI and a chase method if the player aggros the enemy
+
 @onready var hurt_particles : GPUParticles2D = $HurtParticles
 
 const GRAVITY = 200
+const SPEED = 200
 var attack_power : int = 10
+var chase_player : bool = false
 
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
+	
+	if chase_player:
+		var direction = (Globals.player_pos - position).normalized()
+		velocity.x = direction.x * SPEED
 	
 	move_and_slide()
 
@@ -17,6 +25,7 @@ func _on_health_component_on_death():
 func _on_hitbox_component_area_entered(area):
 	if area.has_method("damage"):
 		area.damage(attack_power)
+		chase_player = true
 
 
 func _on_health_component_on_health_change(health_change_type):
